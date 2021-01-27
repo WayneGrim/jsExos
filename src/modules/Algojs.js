97eb1692a1c16256c24cce5e717f7bdd30ebd971
Wayne Grim <wayne.grimardias.dev@gmail.com>
2021-01-27 09:49:15 +0100

    15 - Array of Multiples

--- src/modules/Algojs.js
+++ src/modules/Algojs.js
@@ -14,10 +14,12 @@
 // export function goalTotal(a,b,c) {
 //    return a + b + c;
 // }
+
 // --------------------------------------------------- Grasshopper Order of Operations -------------------------------------
 // export function GrasshopperOrder () {
 //     return (2 + 2) * (2 + 2) * 2;
 //     }
+
 // --------------------------------------------------- Return the First Element in an Array -------------------------------------
 // export function getFirstValue(tab) {
 //     tab = [58, 25, 15, 45 ,78]
@@ -47,24 +49,24 @@
 // }
 
 // --------------------------------------------------- Filter Strings from Array -------------------------------------
-// export function sortByLength(a){
+// export function sortByLength(a) {
 //     var swapped;
-//     do{
+//     do {
 //         swapped = false
-//         for (var i = 0; i < a.length - 1; i++){
-//             if(a[i].length > a[i+1].length){
+//         for (var i = 0; i < a.length - 1; i++) {
+//             if (a[i].length > a[i + 1].length) {
 //                 var tmp = a[i];
-//                 a[i] = a[i+1];
-//                 a[i+1]=tmp
+//                 a[i] = a[i + 1];
+//                 a[i + 1] = tmp
 //                 swapped = true;
 //             }
 //         }
-//     }while(swapped);
+//     } while (swapped);
 //     return a;
 // }
 
 // --------------------------------------------------- Return Last Item -------------------------------------
-// export function lastItem(string){
+// export function lastItem(string) {
 //     var str = string;
 //     var lastLetter = str[str.length - 1];
 //     return lastLetter;
@@ -72,78 +74,102 @@
 
 
 // --------------------------------------------------- The Farm Problem -------------------------------------
+// export function animals(){
+
+// }
+
 
 // --------------------------------------------------- Reverse an Array -------------------------------------
 // function reverseArray(arr) {
 //     var newArray = [];
 //     for (var i = arr.length - 1; i >= 0; i--) {
-//       newArray.push(arr[i]);
+//         newArray.push(arr[i]);
 //     }
 //     return newArray;
-//   }
+// }
 
 // --------------------------------------------------- Return the Next Number from the Integer Passed -------------------------------------
-// export function addition(n){
+// export function addition(n) {
 //     var n = 9
 //     return n + 1
 // }
 
 // --------------------------------------------------- Boolean to String Conversion -------------------------------------
-// export function boolToString(flag){
-//     if (flag === true){
+// export function boolToString(flag) {
+//     if (flag === true) {
 //         return 'true';
 //     }
-//     else{
+//     else {
 //         return 'false';
 //     }
 // }
 
 
 // --------------------------------------------------- Filter out Strings from an Array -------------------------------------
+// export function filtArrayInt(arr){
+//     return arr.parseInt();
+// }
 
 // --------------------------------------------------- Array of Multiples -------------------------------------
+export function arrayOfMultiples(a, b){
+    var res = [];
+    for(let i = a; i <= a * b; i += a){
+        res.push(i);
+   };
+   return res;
+}
 
 
 // --------------------------------------------------- Convert Hours and Minutes into Seconds -------------------------------------
-// export function convert(a, b){
+// export function convert(a, b) {
 //     var hours = Math.floor(a * 60 * 60);
 //     var minutes = Math.floor(b * 60);
-//     return hours+minutes;
+//     return hours + minutes;
 // }
+
 // --------------------------------------------------- Return Negative -------------------------------------
-// export function returnNegative(n){
-//     if(n>0){
-//         return n = n* -1
+// export function returnNegative(n) {
+//     if (n > 0) {
+//         return n = n * -1
 //     }
-//     else{
+//     else {
 //         return n
 //     }
 // }
+
 // --------------------------------------------------- Simple Loop -------------------------------------
+// export function simpleLoop(){
+
+// }
 
 // --------------------------------------------------- Area of a Rectangle -------------------------------------
-export function getArea(a, b) {
-    if (a > 0 && b > 0) {
-        return a * b
-    }
-    else {
-        return -1
-    }
-}
-// --------------------------------------------------- Two Makes Ten -------------------------------------
-// export function makesTen(a, b){
+// export function getArea(a, b) {
+//     if (a > 0 && b > 0) {
+//         return a * b
+//     }
+//     else {
+//         return -1
+//     }
+// }
 
-//     if (a===10 || b===10 || a + b === 10){
+// --------------------------------------------------- Two Makes Ten -------------------------------------
+// export function makesTen(a, b) {
+//     if (a === 10 || b === 10 || a + b === 10) {
 //         console.log('test', a + b);
 //         return true;
 //     }
-//         else{
-//             return false;
-//         }
+//     else {
+//         return false;
+//     }
 // }
+
 // --------------------------------------------------- Add up the Numbers from a Single Number -------------------------------------
-// export function addUp(n){
+// export function addUp(n) {
 //     let sum = n * (n + 1) / 2;
 //     return sum;
 // }
+
 // --------------------------------------------------- Matchstick Houses -------------------------------------
+// export function matchHouses(){
+
+// }
